c655ea6854e7959fa7294d3b7065b8436f3b2e67
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2023-07-30 20:23:46 +0200

    update

--- src/components/IndiGroup.vue
+++ src/components/IndiGroup.vue
@@ -4,6 +4,7 @@
 import IndiTextVector from './controls/IndiTextVector.vue';
 import IndiNumberVector from './controls/IndiNumberVector.vue';
 import IndiSwitchVector from './controls/IndiSwitchVector.vue';
+import IndiLightVector from './controls/IndiLightVector.vue';
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
@@ -28,6 +29,8 @@ const props = defineProps({
         <indi-number-vector :def-number-vector="property" v-else-if="property['<>'] === 'defNumberVector'" />
 
         <indi-switch-vector :def-switch-vector="property" v-else-if="property['<>'] === 'defSwitchVector'" />
+
+        <indi-light-vector :def-light-vector="property" v-else-if="property['<>'] === 'defLightVector'" />
 
     </div>
 
