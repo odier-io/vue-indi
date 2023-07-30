4050d989dbeba629349fadb47ab86afb6a0f3193
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2023-07-30 22:25:54 +0200

    update

--- src/components/IndiDashboard.vue
+++ src/components/IndiDashboard.vue
@@ -77,17 +77,37 @@ const devices = computed(() => {
 
         </li>
 
+        <li class="nav-item" role="presentation">
+
+            <button class="nav-link px-2 py-1" type="button" data-bs-toggle="tab" data-bs-target="#device_pane_console" role="tab">
+                <i class="bi bi-terminal-fill"></i>
+                Console
+            </button>
+
+        </li>
+
     </ul>
 
     <!-- *********************************************************************************************************** -->
 
     <div class="tab-content mx-lg-5 mx-md-3 mx-0">
 
+        <!-- ******************************************************************************************************* -->
+
         <div :class="`tab-pane fade ${deviceIndex === 0 ? 'show active' : ''}`" :id="`device_pane_${deviceIndex}`" tabindex="0" role="tabpanel" v-for="(deviceInfo, deviceName, deviceIndex) in devices" :key="deviceName">
 
             <indi-device :device-name="deviceName" :device-info="deviceInfo" :device-index="deviceIndex" />
 
         </div>
+
+        <!-- ******************************************************************************************************* -->
+
+        <div class="tab-pane fade" id="device_pane_console" tabindex="0" role="tabpanel">
+
+
+        </div>
+
+        <!-- ******************************************************************************************************* -->
 
     </div>
 
