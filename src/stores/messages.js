5f459300512175203fe6dddc82a02e8d391c4888
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2023-07-31 19:01:15 +0200

    update

--- src/stores/messages.js
+++ src/stores/messages.js
@@ -24,7 +24,7 @@ const useMessageStore = defineStore('messages', {
 
             this.terminalInstance.clear();
 
-            this.terminalInstance.open(device in this.messages ? this.messages[deviceName].map((x) => `${x.timestamp}: ${x.message}`).join('\n') + '\n' : '');
+            this.terminalInstance.write(deviceName in this.messages ? this.messages[deviceName].map((x) => `${x.timestamp}: ${x.message}`).join('\n') + '\n' : '');
         },
     },
 });
