a9fdf299325833d76005a99176dfb2423c187ce3
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2023-07-31 14:31:23 +0200

    update

--- src/components/IndiDashboard.vue
+++ src/components/IndiDashboard.vue
@@ -95,7 +95,7 @@ const devices = computed(() => {
         <li class="nav-item" role="presentation">
 
             <button class="nav-link xxxxxx px-3 py-2" type="button" data-bs-toggle="tab" data-bs-target="#indi_console_pane" role="tab">
-                <i class="bi bi-terminal"></i>
+                <i class="bi bi-card-text"></i>
                 Console
             </button>
 
