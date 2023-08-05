be72fe33ab5d9b758cac41baab988fe4b49b8526
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2023-08-05 23:12:44 +0200

    update

--- src/components/typeahead/typeahead.vue
+++ src/components/typeahead/typeahead.vue
@@ -38,10 +38,11 @@ const updateValue = (e) => {
         <input type="text" :value="modelValue" @input="updateValue" data-bs-toggle="dropdown" />
 
         <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-            <li><a class="dropdown-item" href="#">Separated link</a></li>
+            <li v-for="option in options">
+                <a class="dropdown-item" href="#">
+                    {{option}}
+                </a>
+            </li>
         </ul>
 
     </div>
