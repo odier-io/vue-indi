f8c736c86636adb32cc2fdd0aa397c7c75ca82a3
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2023-08-11 19:54:10 +0200

    update

--- src/stores/indi.js
+++ src/stores/indi.js
@@ -12,10 +12,14 @@ import 'xterm/css/xterm.css';
 const CATEGORY_DEFS = [
     {value: 'dome', label: 'Dome'},
     {value: 'mount', label: 'Mount'},
-    {value: 'imaging_camera', label: 'Imaging camera'},
-    {value: 'guiding_camera', label: 'Guiding camera'},
-    {value: 'focuser', label: 'Focuser'},
-    {value: 'filters', label: 'Filters'},
+    {value: 'imaging_camera1', label: 'Imaging camera 1'},
+    {value: 'guiding_camera1', label: 'Guiding camera 1'},
+    {value: 'focuser1', label: 'Focuser 1'},
+    {value: 'filters1', label: 'Filters 1'},
+    {value: 'imaging_camera2', label: 'Imaging camera 2'},
+    {value: 'guiding_camera2', label: 'Guiding camera 2'},
+    {value: 'focuser2', label: 'Focuser 2'},
+    {value: 'filters2', label: 'Filters 2'},
     {value: 'weather', label: 'Weather'},
 ];
 
