5ed0f8c5457068f930731bbaa8070bef65a0b60d
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2023-08-15 13:16:40 +0200

    update

--- src/components/IndiVariable.vue
+++ src/components/IndiVariable.vue
@@ -36,7 +36,7 @@ const filteredVariables = computed(() => {
 
     <!-- *********************************************************************************************************** -->
 
-    <div class="card mx-auto mb-2">
+    <div class="card mx-auto mb-3">
         <div class="card-header px-3 py-2">
             <i class="bi bi-braces"></i>
             Variables
@@ -45,7 +45,7 @@ const filteredVariables = computed(() => {
 
             <!-- *************************************************************************************************** -->
 
-            <div class="input-group mb-3">
+            <div class="input-group mb-2">
                 <span class="input-group-text">
                     <i class="bi bi-funnel"></i>
                     Filter
@@ -55,7 +55,7 @@ const filteredVariables = computed(() => {
 
             <!-- *************************************************************************************************** -->
 
-            <div class="table-responsive" style="max-height: 400px; overflow-y: scroll;">
+            <div class="table-responsive" style="max-height: 425px; overflow-y: scroll;">
 
                 <table class="table table-sm table-striped">
                     <thead>
