de59d780fc621c7a6a664515615f1c9bd685fe15
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2023-09-08 20:33:22 +0200

    update

--- src/components/dashboard/IndiHome.vue
+++ src/components/dashboard/IndiHome.vue
@@ -44,10 +44,10 @@ const serverStop = () => {
 
                     <div class="btn-group btn-group-sm w-75" role="group">
 
-                        <button :class="!indiStore.isOn ? 'btn btn-outline-secondary' : 'btn btn-primary'" style="width: 50%;" @change="serverStop">
+                        <button :class="indiStore.isOn ? 'btn btn-outline-secondary' : 'btn btn-secondary'" style="width: 50%;" @click="serverStop">
                             Turn off devices
                         </button>
-                        <button :class="indiStore.isOn ? 'btn btn-outline-secondary' : 'btn btn-primary'" style="width: 50%;" @change="serverStart">
+                        <button :class="!indiStore.isOn ? 'btn btn-outline-primary' : 'btn btn-primary'" style="width: 50%;" @click="serverStart">
                             Turn on devices
                         </button>
 
